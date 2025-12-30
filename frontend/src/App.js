37ef11298d016ce041a22a602046e8e83d83db60
Your Name <rsbaghel9314@gmail.com>
2025-12-30 16:08:34 +0530

    Fix: Add catch-all route to handle 404s and import Navigate for proper routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/user-login/login';
 import { ToastContainer } from 'react-toastify';
@@ -54,6 +54,8 @@ function App() {
             <Route path='/setting' element={<Setting />} />
           </Route>
 
+          {/* Catch all unmatched routes */}
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
       </Router>
     </>
